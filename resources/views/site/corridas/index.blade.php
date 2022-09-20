166dc67a9929a0747344259c83073df166a3fcd1
Vinicius Melchior <vmelchior.93@gmail.com>
2022-09-19 22:48:05 -0300

    inserção dos icones de visualização, exclusão, edição e outros nas tela de temporada e corridas

--- resources/views/site/corridas/index.blade.php
+++ resources/views/site/corridas/index.blade.php
@@ -1,33 +1,44 @@
 @extends('layouts.main')
 
 @section('section')
+{{-- <h1>Temporada {{$temporada->ano->ano}}</h1> --}}
   <div class="container mt-3 mb-3">
-    @if(count($corridas) > 0)
-    <table class="table table-sm">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Pista</th>
-                <th>Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($corridas as $key => $corrida)
-                <tr @if($corrida->flg_sprint == 'S') style="color:red;" @endif>
-                    <td>@if($corrida->flg_sprint != 'S') {{$corrida->ordem}} @endif</td>
-                    <td>{{$corrida->pista->nome}} @if($corrida->flg_sprint == 'S') - Sprint @endif</td>
-                    <td>
-                        <a href="{{route('resultados.show', [$corrida->id])}}">Visualizar</a>
-                        <a class="" href="{{route('resultados.edit', [$corrida->id])}}">Inserir Resultados</a>
-                        <a class="" href="{{route('corridas.delete', [$corrida->id])}}">Excluir</a>
-                    </td>
+
+    <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Temporada {{$temporada->ano->ano}}</li>
+        </ol>
+    </nav>
+
+    <div class="left_table">
+        @if(count($corridas) > 0)
+        <table class="table" id="tabelaCorridas">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Pista</th>
+                    <th>Ações</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-    @else
-        <p>Nenhuma corrida cadastrada</p>
-    @endif
-    <a href="{{route('corridas.create')}}" class="btn btn-primary">Adicionar Corridas</a>
+            </thead>
+            <tbody>
+                @foreach ($corridas as $key => $corrida)
+                    <tr @if($corrida->flg_sprint == 'S') style="color:red;" @endif>
+                        <td>@if($corrida->flg_sprint != 'S') {{$corrida->ordem}} @endif</td>
+                        <td>{{$corrida->pista->nome}} @if($corrida->flg_sprint == 'S') - Sprint @endif</td>
+                        <td class="d-flex" style="justify-content: space-around;">
+                            <a data-toggle="tooltip" data-placement="top" title="Visualizar resultados" href="{{route('resultados.show', [$corrida->id])}}"><i class="bi bi-eye-fill"></i></a>
+                            <a data-toggle="tooltip" data-placement="top" title="Inserir Resultados" class="" href="{{route('resultados.edit', [$corrida->id])}}"><i class="bi bi-plus-circle-fill"></i></a>
+                            <a data-toggle="tooltip" data-placement="top" title="Deletar" class="" href="{{route('corridas.delete', [$corrida->id])}}"><i class="bi bi-trash-fill"></i></a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        @else
+            <p>Nenhuma corrida cadastrada</p>
+        @endif
+    </div>
+    <a href="{{route('corridas.adicionar', [$temporada->id])}}" class="btn btn-primary">Adicionar Corridas</a>
   </div>
 @endsection
--- resources/views/site/temporadas/index.blade.php
+++ resources/views/site/temporadas/index.blade.php
@@ -2,36 +2,46 @@
 
 @section('section')
   <div class="container mt-3 mb-3">
-    @if(count($temporadas) > 0)
-    <table class="table table-sm">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Descrição</th>
-                <th>Ano</th>
-                <th>ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($temporadas as $key => $temporada)
+
+    <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Temporadas</li>
+        </ol>
+    </nav>
+
+    <div class="left_table">
+        @if(count($temporadas) > 0)
+        <table class="table table-sm" id="tabelaTemporadas">
+            <thead>
                 <tr>
-                    <td>{{$key+1}}</td>
-                    <td>{{$temporada->des_temporada}}</td>
-                    <td>{{$temporada->ano->ano}}</td>
-                    <td>
-                        <a href="{{route('temporadas.edit', [$temporada->id])}}">Classificação</a>
-                        <a href="{{route('temporadas.edit', [$temporada->id])}}">Editar</a>
-                        <a class="" href="{{route('temporadas.delete', [$temporada->id])}}">Excluir</a>
-                        <a href="{{route('corridas.index', [$temporada->id])}}">Visualizar Corridas</a>
-                        <a class="" href="{{route('corridas.adicionar', [$temporada->id])}}">Adicionar Corridas</a>
-                    </td>
+                    <th>#</th>
+                    <th>Descrição</th>
+                    <th>Ano</th>
+                    <th>ações</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-    @else
-        <p>Nenhuma pista cadastrada</p>
-    @endif
+            </thead>
+            <tbody>
+                @foreach ($temporadas as $key => $temporada)
+                    <tr>
+                        <td>{{$key+1}}</td>
+                        <td>{{$temporada->des_temporada}}</td>
+                        <td>{{$temporada->ano->ano}}</td>
+                        <td class="d-flex" style="justify-content: space-between;">
+                            <a data-toggle="tooltip" data-placement="top" title="Classificação" href="{{route('temporadas.edit', [$temporada->id])}}"><i class="bi bi-table"></i></a>
+                            <a data-toggle="tooltip" data-placement="top" title="Editar Temporada" href="{{route('temporadas.edit', [$temporada->id])}}"><i class="bi bi-pencil-fill"></i></a>
+                            <a data-toggle="tooltip" data-placement="top" title="Visualizar Corridas" href="{{route('corridas.index', [$temporada->id])}}"><i class="bi bi-eye-fill"></i></a>
+                            <a data-toggle="tooltip" data-placement="top" title="Adicionar Corridas" class="" href="{{route('corridas.adicionar', [$temporada->id])}}"><i class="bi bi-plus-circle-fill"></i></a>
+                            <a data-toggle="tooltip" data-placement="top" title="Deletar" class="" href="{{route('temporadas.delete', [$temporada->id])}}"><i class="bi bi-trash-fill"></i></a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        @else
+            <p>Nenhuma pista cadastrada</p>
+        @endif
+    </div>
     <a href="{{route('temporadas.create')}}" class="btn btn-primary">Adicionar temporada</a>
   </div>
 @endsection
