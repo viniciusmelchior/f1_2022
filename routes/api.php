149cf3d83cf992efe604f1787a0f4c877ab3a630
Vinicius Melchior <vmelchior.93@gmail.com>
2022-11-02 12:29:49 -0300

    adição de rota de api para exibir classificação

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Site\Piloto;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +18,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/testes', function (Request $request) {
+    return Piloto::with('pais')->where('user_id', 1)->get();
+});
+
+Route::get('/classificacao', function (Request $request) {
+            return DB::select('select piloto_id, concat(pilotos.nome, " ", pilotos.sobrenome) as nome, equipes.nome as equipe, sum(pontuacao) as total from resultados
+            join piloto_equipes on piloto_equipes.id = resultados.pilotoEquipe_id
+            join pilotos on pilotos.id = piloto_equipes.piloto_id
+            join equipes on equipes.id = piloto_equipes.equipe_id
+            join corridas on corridas.id = resultados.corrida_id
+            join temporadas on temporadas.id = corridas.temporada_id
+            where temporadas.id = 1
+            and resultados.user_id = 1
+            group by piloto_equipes.piloto_id
+            order by total desc');
 });
