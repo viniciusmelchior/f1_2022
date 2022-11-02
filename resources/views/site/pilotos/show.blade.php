0f6a55cc8851df077bb2a8906b0d5d6c7d5e7d18
Vinicius Melchior <vmelchior.93@gmail.com>
2022-11-02 17:51:35 -0300

    Ajustes na tabelas e graficos na tela de exibição dos pilotos

--- resources/views/site/pilotos/show.blade.php
+++ resources/views/site/pilotos/show.blade.php
@@ -11,155 +11,229 @@
     text-align: center;
     }
     
-    table, th, td {
-    border: 1px solid black;
-    }
-    
     table {
-    border-collapse: collapse;
-    margin: auto;
+        border-collapse: collapse;
+        margin: auto;
     }
     
-    th, td{
-    padding: 10px;
-    text-align: center!important;
-    width: 190px;
+    #driver-container{
+        /* border: 1px solid black; */
+        margin-top: 5%;
+        display: flex;
     }
-    
-    th{
-    font-weight: bold;
+
+    #driver-details{
+        background-color: #73b2959c;
+        width: 35%;
+        padding: 2%;
+        display: flex;
     }
-    
-    
-    tr:nth-child(even) {
-    background-color: #dce6eb;
+
+    #driver-details h4{
+        font-size: 16px;
+        font-weight: lighter;
+        margin-bottom: 0;
     }
-    
-    tr:hover:nth-child(1n + 2) {
-    background-color: #a0200f;
-    color: #fff;
-    }
-    
-    .header-tabelas{
-        padding: 15px;
-        background-color: rgba(194, 26, 26, 0.993);
-        text-align: center;
-        font-size: 25px;
+
+    #driver-details p{
+        font-size: 24px;
         font-weight: bolder;
-        color: white;
     }
 
     .image-wrapper{
-        max-width: 250px;
+        max-width: 200px;
         margin: auto;
+    }
+
+    #driver-item-details{
+        display: flex;
     }
 
     .image-wrapper img{
         max-width: 100%;
         border-radius: 2%;
     }
+
+    #driver-stats{
+        background-color: #ebc83aaf;
+        width: 65%;
+        padding: 2%;
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: repeat(2, 1fr);
+        grid-column-gap: 0px;
+        grid-row-gap: 0px;
+    }
+
+    #driver-stats div{
+        width: 150px;
+        height: 100px;
+        margin-right: 25px;
+        margin-bottom: 25px;
+    }
+
+    #driver-stats h4{
+        font-size: 18px;
+        margin-bottom: 0;
+        font-weight: lighter;
+        text-transform: uppercase;
+    }
+
+    #driver-stats p{
+        font-size: 26px;
+        font-weight: bolder;
+        text-transform: uppercase;
+        margin-bottom: 0;
+    }
+
+    .other-stats{
+        display: none;
+    }
+
+    .resultados-por-corrida{
+        margin-top: 35px;
+    }
+
+    .resultados-por-corrida h1 {
+        text-transform: uppercase;
+    }
+
+    .tabela-resultados {
+        border-collapse: collapse;
+        width: 60%;
+    }
+
+    .tabela-resultados th, td{
+        padding: 8px;
+        text-align: left;
+        border-bottom: 1px solid #ddd;
+    }
+
+    .tabela-resultados tr:hover {
+        background-color: #73b2959c;
+    }
+
+    .tabela-resultados th{
+        text-transform: uppercase;
+    }
     
 </style>
    <div class="container">
-    {{-- <h1 class="mt-3">{{$modelPiloto->nome}} {{$modelPiloto->sobrenome}}</h1> --}}
-    <div class="image-wrapper mt-3">
-        <img src="{{asset('images/'.$modelPiloto->imagem)}}" alt="">
+
+    <div id="driver-container">
+        <div id="driver-details">
+            <div>
+                <div>
+                    <div>
+                        <h4>Nome do Piloto</h4>
+                        <p>{{ $modelPiloto->nomeCompleto() }}</p>
+                    </div>
+                    <div>
+                        <h4>País/Região</h4>
+                        <p>{{ $modelPiloto->pais->des_nome }}</p>
+                    </div>
+                    <div>
+                        <h4>Status</h4>
+                        @if($modelPiloto->flg_ativo == 'S')
+                            <p>Em Atividade</p>
+                        @else 
+                            <p>Aposentado</p>
+                        @endif
+                    </div>
+                    <div>
+                        <h4>Corridas Disputadas</h4>
+                        <p>{{ $totCorridas }}</p>
+                    </div>
+                    <div>
+                        <button id="show-other-stats" class="btn btn-primary">Exibir Mais Estatisticas</button>
+                    </div>
+                </div>
+            </div>
+            <div class="image-wrapper mt-3">
+                <img src="{{asset('images/'.$modelPiloto->imagem)}}" alt="">
+            </div>
+        </div>
+        <div id="driver-stats">
+           <div>
+                <h4>Campeonato de Pilotos</h4>
+                <p>0</p>
+           </div>
+            <div>
+                <h4>Vitórias</h4>
+                <p>{{ $totVitorias }}</p>
+           </div>
+            <div>
+                <h4>Pole Positions</h4>
+                <p>{{ $totPoles }}</p>
+           </div>
+            <div>
+                <h4>Subidas ao Pódio</h4>
+                <p>{{ $totPodios }}</p>
+           </div>
+            <div>
+                <h4>Total de Pontos</h4>
+                <p>{{ $totPontos }}</p>
+           </div>
+            <div>
+                <h4>Voltas mais rapidas</h4>
+                <p>{{ $totVoltasRapidas }}</p>
+           </div>
+            <div class="other-stats">
+                <h4>Chegadas no top 10</h4>
+                <p>{{ $totTopTen }}</p>
+           </div>
+            <div class="other-stats">
+                <h4>Melhor largada</h4>
+                <p>{{ $melhorPosicaoLargada }}º</p>
+           </div>
+            <div class="other-stats">
+                <h4>Pior Largada</h4>
+                <p>{{ $piorPosicaoLargada }}º</p>
+           </div>
+            <div class="other-stats">
+                <h4>Melhor Chegada</h4>
+                <p>{{ $melhorPosicaoChegada }}º</p>
+           </div>
+            <div class="other-stats">
+                <h4>pior Chegada</h4>
+                <p>{{ $piorPosicaoChegada }}º</p>
+           </div>
+        </div>
     </div>
-        <table class="mt-2 mb-5">
-            <tr>
-                <th colspan="2">{{$modelPiloto->nome}} {{$modelPiloto->sobrenome}}</th>
-            </tr>
-            <tr>
-                <td>Total De Corridas</td>
-                <td>{{$totCorridas}}</td>
-            </tr>
-            <tr>
-                <td>Vitórias</td>
-                <td>{{$totVitorias}}</td>
-            </tr>
-            <tr>
-                <td>Pole Positions</td>
-                <td>{{$totPoles}}</td>
-            </tr>
-            <tr>
-                <td>Pódios</td>
-                <td>{{$totPodios}}</td>
-            </tr>
-            <tr>
-                <td>Total de Pontos</td>
-                <td>{{$totPontos}}</td>
-            </tr>
-            <tr>
-                <td>Chegadas no Top 10</td>
-                <td>{{$totTopTen}}</td>
-            </tr>
-            <tr>
-                <td>Melhor Largada</td>
-                <td>{{$melhorPosicaoLargada}}</td>
-            </tr>
-            <tr>
-                <td>Pior Largada</td>
-                <td>{{$piorPosicaoLargada}}</td>
-            </tr>
-            <tr>
-                <td>Melhor Chegada</td>
-                <td>{{$melhorPosicaoChegada}}</td>
-            </tr>
-            <tr>
-                <td>Pior Chegada</td>
-                <td>{{$piorPosicaoChegada}}</td>
-            </tr>
-            <tr>
-                <td>Voltas Mais Rápidas</td>
-                <td>{{$totVoltasRapidas}}</td>
-            </tr>
-            {{-- <tr>
-                <td>Abandonos</td>
-                <td>0</td>
-            </tr> --}}
-            <tr>
-                <td>Status</td>
-                <td>
-                    @if($modelPiloto->flg_ativo == 'S')
-                        Em Atividade
-                    @else 
-                        Aposentado
-                    @endif
-                </td>
-            </tr>
-        </table>
 
         @php 
             $resultados = Resultado::where('user_id', Auth::user()->id)->get();
         @endphp
 
         {{--Tabela de Largada e chegada--}}
-        <h1>Resultados por Corrida</h1>
-        <table class="mt-5 mb-5">
-            <tr>
-                <th></th>
-                <th>Largada</th>
-                <th>Chegada</th>
-                <th>Variação</th>
-            </tr>
-            @foreach($resultados as $resultado)
+        <section class="resultados-por-corrida">
+            <h1>Resultados por Corrida</h1>
+            <table class="mt-5 mb-5 tabela-resultados">
                 <tr>
-                    @if($resultado->pilotoEquipe->piloto->id == $modelPiloto->id)
-                        <td>{{$resultado->corrida->pista->nome}} - {{$resultado->corrida->temporada->ano->ano}}</td>
-                        <td>{{$resultado->largada}}</td>
-                        <td>{{$resultado->chegada}}</td>
-                        <td>{{$resultado->largada-$resultado->chegada}}</td>
-                    @endif
+                    <th>Temporada</th>
+                    <th>Pista</th>
+                    <th>Largada</th>
+                    <th>Chegada</th>
+                    <th>Variação</th>
                 </tr>
-            @endforeach
-        </table>
-
-        {{--Gráficos--}}
+                @foreach($resultados as $resultado)
+                    <tr @if($resultado->corrida->flg_sprint == 'S') style="font-style:italic; color:red;" @endif>
+                        @if($resultado->pilotoEquipe->piloto->id == $modelPiloto->id)
+                            <td> {{$resultado->corrida->temporada->ano->ano}} </td>
+                            <td>{{$resultado->corrida->pista->nome}} @if($resultado->corrida->flg_sprint == 'S') - Sprint @endif</td>
+                            <td>{{$resultado->largada}}</td>
+                            <td>{{$resultado->chegada}}</td>
+                            <td>{{$resultado->largada-$resultado->chegada}}</td>
+                        @endif
+                    </tr>
+                @endforeach
+            </table>
+        </section>
+       
         <div>
             <canvas id="myChart"></canvas>
         </div>
 
-        {{--Graficos Final--}}
         <div class="mb-5">
             <div class="d-flex" style="justify-content: space-around;">
                 <div class="">
@@ -173,10 +247,11 @@
    </div>
 
    @php 
+
     $chegada = [];
     $labels = [];
     foreach($resultados as $resultado){
-        if($resultado->pilotoEquipe->piloto->id == $modelPiloto->id){
+        if($resultado->pilotoEquipe->piloto->id == $modelPiloto->id && $resultado->corrida->flg_sprint == 'N'){
             array_push($chegada, $resultado->chegada);
             array_push($labels, $resultado->corrida->pista->nome);
         }
@@ -184,7 +259,7 @@
 
     $largada = [];
     foreach($resultados as $resultado){
-        if($resultado->pilotoEquipe->piloto->id == $modelPiloto->id){
+        if($resultado->pilotoEquipe->piloto->id == $modelPiloto->id && $resultado->corrida->flg_sprint == 'N'){
             array_push($largada, $resultado->largada);
         }
     }
@@ -245,7 +320,18 @@
           }
       }
   });
-  
+
+  $('#show-other-stats').click(function (e) { 
+    e.preventDefault();
+    if( this.innerHTML === 'Exibir Mais Estatisticas'){
+        this.innerHTML = 'Esconder Estatisticas';
+    }else{
+        this.innerHTML = 'Exibir Mais Estatisticas'
+    }
+
+    $('.other-stats').toggle();
+  });
+
   </script>
 @endsection
 
