ac9846542f2e511cbd5caeb9f8d4ac67b1d92293
Vinicius Melchior <vmelchior.93@gmail.com>
2023-11-29 09:10:51 -0300

    Reorganização do arquivo JS - Tabelas

--- public/js/home/index.js
+++ public/js/home/index.js
@@ -148,4 +148,196 @@ $(document).ready(function () {
             }
         });
     });
+
+    /* montagem da tabela de vitórias dos pilotos por temporada */
+    $('#vitoriasPilotosPorTemporada').change(function (e) { 
+        e.preventDefault();
+
+        vitoriasPilotosTemporadaId = $('#vitoriasPilotosPorTemporada').val();
+        tabelaVitoriasPilotos = $('#tabelaVitoriasPilotos');
+        tabelaVitoriasPilotos.html('');
+        tabelaVitoriasPilotos.append('<tr><th>#</th><th>Piloto</th><th>Vitórias</th></tr>')
+
+        selectTemporadaVitoriasPiloto = $('#selectTemporadaVitoriasPiloto').text('Selecione uma Temporada');
+
+        if(vitoriasPilotosTemporadaId != ''){
+            selectTemporadaVitoriasPiloto = $('#selectTemporadaVitoriasPiloto').text('Geral');
+        }
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $.ajax({
+            type: "POST",
+            url: ajaxGetVitoriasPilotoPorTemporada,
+            data: {vitoriasPilotosTemporadaId: vitoriasPilotosTemporadaId},
+            contentType: "application/x-www-form-urlencoded;charset=UTF-8",
+            success: function (response) {
+                response.totPorPiloto.forEach(function(piloto, index) {
+                    // tabelaVitoriasPilotos.append("<tr><td><a href='visualizarVitoriasPiloto'><i class='bi bi-eye'></i></a></td><td>"+piloto.nome+"</td><td>"+piloto.vitorias+" id: "+piloto.id+"</td></tr>");
+                    tabelaVitoriasPilotos.append("<tr><td>#</td><td>"+piloto.nome+"</td><td>"+piloto.vitorias);
+                }); 
+            },
+            error:function(){
+                alert(error)
+            }
+        });
+    });
+
+    /* montagem da tabela de vitórias das equipes por temporada */
+    $('#vitoriasEquipesPorTemporada').change(function (e) { 
+        e.preventDefault();
+
+        vitoriasEquipesTemporadaId = $('#vitoriasEquipesPorTemporada').val();
+        tabelaVitoriasEquipes = $('#tabelaVitoriasEquipes');
+        tabelaVitoriasEquipes.html('');
+        tabelaVitoriasEquipes.append('<tr><th>#</th><th>Equipe</th><th>Vitórias</th></tr>')
+
+        selectTemporadaVitoriasEquipes = $('#selectTemporadaVitoriasEquipes').text('Selecione uma Temporada');
+
+        if(vitoriasEquipesTemporadaId != ''){
+            selectTemporadaVitoriasEquipes = $('#selectTemporadaVitoriasEquipes').text('Geral');
+        }
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $.ajax({
+            type: "POST",
+            url: ajaxGetVitoriasEquipesPorTemporada,
+            data: {vitoriasEquipesTemporadaId: vitoriasEquipesTemporadaId},
+            contentType: "application/x-www-form-urlencoded;charset=UTF-8",
+            success: function (response) {
+                for (const key in response.totVitoriasPorEquipe) {
+                    // console.log(`${key}: ${response.totPorPiloto[key]}`);
+                    tabelaVitoriasEquipes.append("<tr><td>#</td><td>"+key+"</td><td>"+response.totVitoriasPorEquipe[key]+"</td></tr>");
+                }
+            },
+            error:function(){
+                alert(error)
+            }
+        });
+    });
+
+    //montagem da tabela de pole position dos pilotos por temporada
+    $('#PolesPilotosPorTemporada').change(function (e) { 
+        e.preventDefault();
+
+        polesPilotosTemporadaId = $('#PolesPilotosPorTemporada').val();
+        console.log(polesPilotosTemporadaId)
+        tabelaPolesPilotos = $('#tabelaPolesPilotos');
+        tabelaPolesPilotos.html('');
+        tabelaPolesPilotos.append('<tr><th>#</th><th>Piloto</th><th>Poles</th></tr>')
+
+        selectTemporadaPolesPiloto = $('#selectTemporadaPolesPilotos').text('Selecione uma Temporada');
+
+        if(polesPilotosTemporadaId != ''){
+            selectTemporadaPolesPiloto = $('#selectTemporadaPolesPilotos').text('Geral');
+        }
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $.ajax({
+            type: "POST",
+            url: ajaxGetPolesPilotosPorTemporada,
+            data: {polesPilotosTemporadaId: polesPilotosTemporadaId},
+            contentType: "application/x-www-form-urlencoded;charset=UTF-8",
+            success: function (response) {
+                for (const key in response.totPolesPorPiloto) {
+                    // console.log(`${key}: ${response.totPorPiloto[key]}`);
+                    tabelaPolesPilotos.append("<tr><td>#</td><td>"+key+"</td><td>"+response.totPolesPorPiloto[key]+"</td></tr>");
+                }
+            },
+            error:function(){
+                alert(error)
+            }
+        });
+    });
+
+    //montagem da tabela de pole position das equipes por temporada
+    $('#PolesEquipesPorTemporada').change(function (e) { 
+        e.preventDefault();
+
+        polesEquipesTemporadaId = $('#PolesEquipesPorTemporada').val();
+        console.log(polesEquipesTemporadaId)
+        tabelaPolesEquipes = $('#tabelaPolesEquipes');
+        tabelaPolesEquipes.html('');
+        tabelaPolesEquipes.append('<tr><th>#</th><th>Piloto</th><th>Poles</th></tr>')
+
+        selectTemporadaPolesEquipes = $('#selectTemporadaPolesEquipes').text('Selecione uma Temporada');
+
+        if(polesEquipesTemporadaId != ''){
+            selectTemporadaPolesEquipe = $('#selectTemporadaPolesEquipes').text('Geral');
+        }
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $.ajax({
+            type: "POST",
+            url: ajaxGetPolesEquipesPorTemporada,
+            data: {polesEquipesTemporadaId: polesEquipesTemporadaId},
+            contentType: "application/x-www-form-urlencoded;charset=UTF-8",
+            success: function (response) {
+                for (const key in response.totPolesPorEquipe) {
+                    // console.log(`${key}: ${response.totPorPiloto[key]}`);
+                    tabelaPolesEquipes.append("<tr><td>#</td><td>"+key+"</td><td>"+response.totPolesPorEquipe[key]+"</td></tr>");
+                }
+            },
+            error:function(){
+                alert(error)
+            }
+        });
+    });
+
+    /* montagem da tabela de podios por piloto por temporada*/
+    $('#podiosPilotosPorTemporada').change(function (e) { 
+        e.preventDefault();
+
+        podiosPilotosTemporadaId = $('#podiosPilotosPorTemporada').val();
+        tabelaPodiosPilotos = $('#tabelaPodiosPilotos');
+        tabelaPodiosPilotos.html('');
+        tabelaPodiosPilotos.append('<tr><th>#</th><th>Piloto</th><th>Podios</th></tr>')
+
+        selectTemporadaPodiosPiloto = $('#selectTemporadaPodiosPilotos').text('Selecione uma Temporada');
+
+        if(podiosPilotosTemporadaId != ''){
+            selectTemporadaPodiosPiloto = $('#selectTemporadaPodiosPilotos').text('Geral');
+        }
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $.ajax({
+            type: "POST",
+            url: ajaxGetPodiosPilotoPorTemporada,
+            data: {podiosPilotosTemporadaId: podiosPilotosTemporadaId},
+            contentType: "application/x-www-form-urlencoded;charset=UTF-8",
+            success: function (response) {
+            response.totPorPiloto.forEach(function(piloto, index) {
+                    tabelaPodiosPilotos.append("<tr><td>#</td><td>"+piloto.nome+"</td><td>"+piloto.podios);
+                }); 
+            },
+            error:function(){
+                alert(error)
+            }
+        });
+    });
+    
 });
--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -672,19 +672,23 @@
     <div class="full-table-pagination-wrapper">
         <div id="pagination" class="pagination"></div>
     </div>
+
     @php 
         // sleep(1);
         // $time = microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"];
         // $tempoExecucao =  "Tempo de execução: ".$time;
         // dd($tempoExecucao);
     @endphp
+
     </div>
 @endsection
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer">
 </script>
 
 <script src="{{asset('js/home/index.js')}}"></script>
 
+{{-- URL's utilizadas para as chamadas AJAX no arquivo Javascript--}}
 <script>
     urlclassificacaoGeralPorTemporada = "<?=route('ajax.classificacaoGeralPorTemporada')?>"
     ajaxGetVitoriasPilotoPorTemporada = "<?=route('ajax.ajaxGetVitoriasPilotoPorTemporada')?>"
@@ -692,200 +696,4 @@
     ajaxGetPolesPilotosPorTemporada = "<?=route('ajax.ajaxGetPolesPilotosPorTemporada')?>"
     ajaxGetPolesEquipesPorTemporada = "<?=route('ajax.ajaxGetPolesEquipesPorTemporada')?>"
     ajaxGetPodiosPilotoPorTemporada = "<?=route('ajax.ajaxGetPodiosPilotoPorTemporada')?>"
-</script>
-
-<script>
-   $(document).ready(function () {
-
-/*montagem da tabela de vitórias dos pilotos por temporada*/
-    $('#vitoriasPilotosPorTemporada').change(function (e) { 
-        e.preventDefault();
-
-        vitoriasPilotosTemporadaId = $('#vitoriasPilotosPorTemporada').val();
-        tabelaVitoriasPilotos = $('#tabelaVitoriasPilotos');
-        tabelaVitoriasPilotos.html('');
-        tabelaVitoriasPilotos.append('<tr><th>#</th><th>Piloto</th><th>Vitórias</th></tr>')
-
-        selectTemporadaVitoriasPiloto = $('#selectTemporadaVitoriasPiloto').text('Selecione uma Temporada');
-
-        if(vitoriasPilotosTemporadaId != ''){
-            selectTemporadaVitoriasPiloto = $('#selectTemporadaVitoriasPiloto').text('Geral');
-        }
-
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-
-        $.ajax({
-            type: "POST",
-            url: ajaxGetVitoriasPilotoPorTemporada,
-            data: {vitoriasPilotosTemporadaId: vitoriasPilotosTemporadaId},
-            contentType: "application/x-www-form-urlencoded;charset=UTF-8",
-            success: function (response) {
-                response.totPorPiloto.forEach(function(piloto, index) {
-                    // tabelaVitoriasPilotos.append("<tr><td><a href='visualizarVitoriasPiloto'><i class='bi bi-eye'></i></a></td><td>"+piloto.nome+"</td><td>"+piloto.vitorias+" id: "+piloto.id+"</td></tr>");
-                    tabelaVitoriasPilotos.append("<tr><td>#</td><td>"+piloto.nome+"</td><td>"+piloto.vitorias);
-                }); 
-            },
-            error:function(){
-                alert(error)
-            }
-        });
-    });
-
-    /*montagem da tabela de vitórias dos pilotos por temporada*/
-    $('#vitoriasEquipesPorTemporada').change(function (e) { 
-        e.preventDefault();
-
-        vitoriasEquipesTemporadaId = $('#vitoriasEquipesPorTemporada').val();
-        tabelaVitoriasEquipes = $('#tabelaVitoriasEquipes');
-        tabelaVitoriasEquipes.html('');
-        tabelaVitoriasEquipes.append('<tr><th>#</th><th>Equipe</th><th>Vitórias</th></tr>')
-
-        selectTemporadaVitoriasEquipes = $('#selectTemporadaVitoriasEquipes').text('Selecione uma Temporada');
-
-        if(vitoriasEquipesTemporadaId != ''){
-            selectTemporadaVitoriasEquipes = $('#selectTemporadaVitoriasEquipes').text('Geral');
-        }
-
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-
-        $.ajax({
-            type: "POST",
-            url: ajaxGetVitoriasEquipesPorTemporada,
-            data: {vitoriasEquipesTemporadaId: vitoriasEquipesTemporadaId},
-            contentType: "application/x-www-form-urlencoded;charset=UTF-8",
-            success: function (response) {
-                for (const key in response.totVitoriasPorEquipe) {
-                    // console.log(`${key}: ${response.totPorPiloto[key]}`);
-                    tabelaVitoriasEquipes.append("<tr><td>#</td><td>"+key+"</td><td>"+response.totVitoriasPorEquipe[key]+"</td></tr>");
-                }
-            },
-            error:function(){
-                alert(error)
-            }
-        });
-    });
-
-    //montagem da tabela de pole position dos pilotos por temporada
-    $('#PolesPilotosPorTemporada').change(function (e) { 
-        e.preventDefault();
-
-        polesPilotosTemporadaId = $('#PolesPilotosPorTemporada').val();
-        console.log(polesPilotosTemporadaId)
-        tabelaPolesPilotos = $('#tabelaPolesPilotos');
-        tabelaPolesPilotos.html('');
-        tabelaPolesPilotos.append('<tr><th>#</th><th>Piloto</th><th>Poles</th></tr>')
-
-        selectTemporadaPolesPiloto = $('#selectTemporadaPolesPilotos').text('Selecione uma Temporada');
-
-        if(polesPilotosTemporadaId != ''){
-            selectTemporadaPolesPiloto = $('#selectTemporadaPolesPilotos').text('Geral');
-        }
-
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-
-        $.ajax({
-            type: "POST",
-            url: ajaxGetPolesPilotosPorTemporada,
-            data: {polesPilotosTemporadaId: polesPilotosTemporadaId},
-            contentType: "application/x-www-form-urlencoded;charset=UTF-8",
-            success: function (response) {
-                for (const key in response.totPolesPorPiloto) {
-                    // console.log(`${key}: ${response.totPorPiloto[key]}`);
-                    tabelaPolesPilotos.append("<tr><td>#</td><td>"+key+"</td><td>"+response.totPolesPorPiloto[key]+"</td></tr>");
-                }
-            },
-            error:function(){
-                alert(error)
-            }
-        });
-    });
-
-    //montagem da tabela de pole position das equipes por temporada
-    $('#PolesEquipesPorTemporada').change(function (e) { 
-        e.preventDefault();
-
-        polesEquipesTemporadaId = $('#PolesEquipesPorTemporada').val();
-        console.log(polesEquipesTemporadaId)
-        tabelaPolesEquipes = $('#tabelaPolesEquipes');
-        tabelaPolesEquipes.html('');
-        tabelaPolesEquipes.append('<tr><th>#</th><th>Piloto</th><th>Poles</th></tr>')
-
-        selectTemporadaPolesEquipes = $('#selectTemporadaPolesEquipes').text('Selecione uma Temporada');
-
-        if(polesEquipesTemporadaId != ''){
-            selectTemporadaPolesEquipe = $('#selectTemporadaPolesEquipes').text('Geral');
-        }
-
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-
-        $.ajax({
-            type: "POST",
-            url: ajaxGetPolesEquipesPorTemporada,
-            data: {polesEquipesTemporadaId: polesEquipesTemporadaId},
-            contentType: "application/x-www-form-urlencoded;charset=UTF-8",
-            success: function (response) {
-                for (const key in response.totPolesPorEquipe) {
-                    // console.log(`${key}: ${response.totPorPiloto[key]}`);
-                    tabelaPolesEquipes.append("<tr><td>#</td><td>"+key+"</td><td>"+response.totPolesPorEquipe[key]+"</td></tr>");
-                }
-            },
-            error:function(){
-                alert(error)
-            }
-        });
-    });
-
-    /*podios por piloto a cada temporada*/
-    $('#podiosPilotosPorTemporada').change(function (e) { 
-        e.preventDefault();
-
-        podiosPilotosTemporadaId = $('#podiosPilotosPorTemporada').val();
-        tabelaPodiosPilotos = $('#tabelaPodiosPilotos');
-        tabelaPodiosPilotos.html('');
-        tabelaPodiosPilotos.append('<tr><th>#</th><th>Piloto</th><th>Podios</th></tr>')
-
-        selectTemporadaPodiosPiloto = $('#selectTemporadaPodiosPilotos').text('Selecione uma Temporada');
-
-        if(podiosPilotosTemporadaId != ''){
-            selectTemporadaPodiosPiloto = $('#selectTemporadaPodiosPilotos').text('Geral');
-        }
-
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-
-        $.ajax({
-            type: "POST",
-            url: ajaxGetPodiosPilotoPorTemporada,
-            data: {podiosPilotosTemporadaId: podiosPilotosTemporadaId},
-            contentType: "application/x-www-form-urlencoded;charset=UTF-8",
-            success: function (response) {
-            response.totPorPiloto.forEach(function(piloto, index) {
-                    tabelaPodiosPilotos.append("<tr><td>#</td><td>"+piloto.nome+"</td><td>"+piloto.podios);
-                }); 
-            },
-            error:function(){
-                alert(error)
-            }
-        });
-    });
-});
 </script>
