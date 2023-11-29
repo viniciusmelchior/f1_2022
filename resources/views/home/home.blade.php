a3645107f1aff28e2e2c41e3393fbed9958c5d4c
Vinicius Melchior <vmelchior.93@gmail.com>
2023-11-29 08:28:29 -0300

    teste JS

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -683,6 +683,8 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer">
 </script>
 
+<script src="{{asset('js/home/index.js')}}"></script>
+
 <script>
     urlclassificacaoGeralPorTemporada = "<?=route('ajax.classificacaoGeralPorTemporada')?>"
     ajaxGetVitoriasPilotoPorTemporada = "<?=route('ajax.ajaxGetVitoriasPilotoPorTemporada')?>"
