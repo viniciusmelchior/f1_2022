740a2cd9cd71d05887c5652c7f4ea0b07cbd8529
Vinicius Melchior <vmelchior.93@gmail.com>
2023-11-29 10:18:04 -0300

    teste select podios equipes

--- public/js/home/index.js
+++ public/js/home/index.js
@@ -344,6 +344,8 @@ $(document).ready(function () {
      $('#podiosEquipesPorTemporada').change(function (e) { 
         e.preventDefault();
 
+        alert("teste")
+
         podiosEquipesTemporadaId = $('#podiosEquipesPorTemporada').val();
         tabelaPodiosEquipes = $('#tabelaPodiosEquipes');
         tabelaPodiosEquipes.html('');
