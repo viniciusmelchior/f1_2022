fd9d4ed5d4e6f89918b20e2e4450bee2a8926fc9
Vinicius Melchior <vmelchior.93@gmail.com>
2023-11-29 08:41:06 -0300

    Reorganização do arquivo JS

--- public/js/home/index.js
+++ public/js/home/index.js
@@ -1,1 +1,39 @@
-alert('bem vindo');
+$(document).ready(function () {
+
+    //Bloco responsável por mostrar e/ou recolher as tabelas de vitórias, poles etc 
+    $('#toggle_vitorias').click(function (e) { 
+        e.preventDefault();
+        $('#div_vitorias').toggleClass('d-none');
+    });
+
+    $('#toggle_poles').click(function (e) { 
+        e.preventDefault();
+        $('#div_poles').toggleClass('d-none');
+    });
+
+    $('#toggle_podios').click(function (e) { 
+        e.preventDefault();
+        $('#div_podios').toggleClass('d-none');
+    });
+
+    $('#toggle_abandonos').click(function (e) { 
+        e.preventDefault();
+        $('#div_abandonos').toggleClass('d-none');
+    });
+
+    $('#toggle_chegadastop10').click(function (e) { 
+        e.preventDefault();
+        $('#div_chegadastop10').toggleClass('d-none');
+    });
+
+    $('#toggle_titulos').click(function (e) { 
+        e.preventDefault();
+        $('#div_titulos').toggleClass('d-none');
+    });
+
+    $('#toggle_classificacao_historica').click(function (e) { 
+        e.preventDefault();
+        $('#div_classificacao_historica').toggleClass('d-none');
+    });
+    
+});
--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -763,42 +763,7 @@
             });
 
         });
-
-        $('#toggle_vitorias').click(function (e) { 
-            e.preventDefault();
-            $('#div_vitorias').toggleClass('d-none');
-        });
-
-        $('#toggle_poles').click(function (e) { 
-            e.preventDefault();
-            $('#div_poles').toggleClass('d-none');
-        });
-
-        $('#toggle_podios').click(function (e) { 
-            e.preventDefault();
-            $('#div_podios').toggleClass('d-none');
-        });
-
-        $('#toggle_abandonos').click(function (e) { 
-            e.preventDefault();
-            $('#div_abandonos').toggleClass('d-none');
-        });
-
-        $('#toggle_chegadastop10').click(function (e) { 
-            e.preventDefault();
-            $('#div_chegadastop10').toggleClass('d-none');
-        });
-
-        $('#toggle_titulos').click(function (e) { 
-            e.preventDefault();
-            $('#div_titulos').toggleClass('d-none');
-        });
-
-        $('#toggle_classificacao_historica').click(function (e) { 
-            e.preventDefault();
-            $('#div_classificacao_historica').toggleClass('d-none');
-        });
-   
+        
     // Paginação da tabela
 
    var table = document.getElementById("tabelaResultadoCorridas");
